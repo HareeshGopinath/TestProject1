433a941e05ef2f4c5b60acc042f00d46a2e74419
hareesh <hareeshg@Hareeshs-MacBook-Pro-3.local>
2016-10-27 11:26:50 +0530

    Project added

---
+++ Myworld_git/SampleHelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleHelloWorld
+//
+//  Created by hareesh  on 27/10/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Myworld_git/SampleHelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleHelloWorld
+//
+//  Created by hareesh  on 27/10/16.
+//  Copyright © 2016 Test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
